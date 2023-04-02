20b1b75a12a1338bf2924b6c1dd885acb11fe32b
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-04-02 08:46:01 -0400

    Delete src directory

--- src/api/auth.js
+++
@@ -1,5 +0,0 @@
-import API from "./index";
-
-export const userLogin = (loginData) => API.post("/login", loginData);
-
-export const registerUser = (signUpData) => API.post("/signup", signUpData);
--- src/providers/stateProvider.js
+++
@@ -1,8 +0,0 @@
-import { Provider } from "react-redux";
-import store from "../flux/store/index";
-
-function StateProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
-}
-
-export default StateProvider;
